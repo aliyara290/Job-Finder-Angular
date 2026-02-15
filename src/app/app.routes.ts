5d670958918618816f369d88f1a3102667a4299d
Ali Yara <ali.yara.fr@gmail.com>
2026-02-15 14:14:09 +0000

    add home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,19 @@ export const routes: Routes = [
   {
     path: "auth",
     loadChildren: () => import("./domains/auth/auth.routing").then((c) => c.AuthRouting)
+  },
+  {
+    path: "",
+    loadComponent: () => import("./core/layout/index/index.component").then((c) => c.IndexComponent),
+    children: [
+      {
+        path: "",
+        loadComponent: () => import("./domains/home/page/page.component").then((c) => c.PageComponent)
+      }
+    ]
+  },
+  {
+    path: "**",
+    redirectTo: ""
   }
 ];
