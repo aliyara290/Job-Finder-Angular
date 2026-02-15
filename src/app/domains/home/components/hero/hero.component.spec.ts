c4fe01aeea5991e0813f5c8e4c68fdc1a24498e5
Ali Yara <ali.yara.fr@gmail.com>
2026-02-15 14:13:00 +0000

    add hero component in the home feature

--- src/app/domains/home/components/hero/hero.component.spec.ts
+++ src/app/domains/home/components/hero/hero.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SecondaryHeaderComponent } from './secondary-header.component';
+import { HeroComponent } from './hero.component';
 
-describe('SecondaryHeaderComponent', () => {
-  let component: SecondaryHeaderComponent;
-  let fixture: ComponentFixture<SecondaryHeaderComponent>;
+describe('HeroComponent', () => {
+  let component: HeroComponent;
+  let fixture: ComponentFixture<HeroComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SecondaryHeaderComponent]
+      imports: [HeroComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SecondaryHeaderComponent);
+    fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
