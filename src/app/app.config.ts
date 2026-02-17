81986e13aec5118c15c26c099d5534bedfdbd0c7
Ali Yara <ali.yara.fr@gmail.com>
2026-02-17 15:08:13 +0000

    Add api-key-interceptor for include the USA-Jobs API api-key, and add it in the provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {apiKeyInterceptor} from './core/interceptors/api-key-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors(
+        [
+          apiKeyInterceptor
+        ]
+      )
+    )
   ]
 };
